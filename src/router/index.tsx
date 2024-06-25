9b6ae1308afc51faf6fe451fd82990b8260cc110
joao-vi <joaovictor.go.ro@gmail.com>
2024-06-25 14:55:53 -0400

    refactor: change NavigationContainer theme

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,32 +1,18 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { Text, View } from 'react-native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { CatalogStack } from '../modules/catalog/routes'
 
-function HomeScreen() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Text style={{ color: 'black' }}>Home Screen</Text>
-    </View>
-  )
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'white',
+  },
 }
-
-const Stack = createNativeStackNavigator()
 
 export const Router = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
-      </Stack.Navigator>
+    <NavigationContainer theme={theme}>
+      <CatalogStack />
     </NavigationContainer>
   )
 }
